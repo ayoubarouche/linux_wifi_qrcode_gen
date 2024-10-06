28b8594343696055eaf361b695a59b6d39b6aa99
ayoubarouche <aarouche2016@gmail.com>
2024-10-06 19:24:19 +0100

    check if python library of qrcode already installed else install it

--- qrcode_gen.sh
+++ qrcode_gen.sh
@@ -1,7 +1,31 @@
 #!/usr/bin/env bash
+
+# check if qrcode already installed or not : 
+qrcode_generator_name="python3-maroc"
+qrcode_installed=$(dpkg -s $qrcode_generator_name  -W -f='${Status}' nano 2>/dev/null | grep -c "ok installed")
 current_directory=$(pwd) 
 test_wifi_name=$(cat test_wifi.wifi)
 cd /etc/NetworkManager/system-connections/
+
+
+if [[ $qrcode_installed == "1" ]]
+     then
+          echo "is installed"
+     else 
+          echo "not installed"
+          echo "trying to install it"
+          sudo apt-get install $qrcode_generator_name -y &> /dev/null
+          # check the installation status :
+          if [[ $? > 0 ]]
+               then 
+               echo "installation failed "
+               echo "exit the script"
+               exit
+               else
+               echo "installation done continue running the script"
+          fi
+          # exit
+fi          
 output=$(ls | grep "$test_wifi_name.nmconnection")
 
 
